69c976980045b532d023483fbfaee1bc763877eb
Felipe Rodrigues Michetti <felipe@codecode.com.br>
2018-02-22 11:55:47 -0300

    Added configuration class and controller.

---
+++ lib/controllers/configuration.rb
@@ -0,0 +1,35 @@
+#
+# Base requirement
+#
+require 'requires'
+require 'base_controller'
+
+module Controllers
+  module ConfiguracaoCtrl
+    class << self
+      def extended(controller)
+        controller.include Utils::ApiHelper
+
+        controller.namespace('/api') {|c|
+          c.get('/configuration/version') {
+            make_default_json_api(self) {
+              Configuration.get_version
+            }
+          }
+
+          c.get('/configuration') {
+            make_default_json_api(self) {
+              Configuration.list_configurations
+            }
+          }
+
+          c.get('/routes') {
+            make_default_json_api(self) {
+              Configuration.list_apis(c)
+            }
+          }
+        }
+      end
+    end
+  end
+end
---
+++ lib/models/configuration.rb
@@ -0,0 +1,54 @@
+require 'delfos/requires'
+
+
+module Models
+  include Models::Base
+
+  # @class [Configuration]
+  class Configuration < BaseModel
+
+    # Set Configuration dataset:
+    set_dataset DB[:configuration]
+
+    # Set primary key and relationships:
+    set_primary_key :id
+
+    # def initialize; end
+
+    class << self
+      def save_configuration(name, value)
+        c = Configuration.new
+        c.name = name
+        c.value = value
+        c.save
+      end
+
+      def get_version
+        conf = Configuration.where(name: 'version').first
+        conf.values
+      end
+
+      def get_configuration(name)
+        conf = Configuration.where(name: name).first
+        conf.values
+      end
+
+      def list_configurations
+        Configuration.all.map(&:values)
+      end
+
+      def list_apis(controller)
+        routes = controller.routes
+        post_routes = routes['POST'].collect {|r| r.first.to_s}
+        get_routes = routes['GET'].collect {|r| r.first.to_s}
+        delete_routes = routes['DELETE'].collect {|r| r.first.to_s}
+        options_routes = routes['OPTIONS'].collect {|r| r.first.to_s}
+        put_routes = routes['PUT'].collect {|r| r.first.to_s}
+        patch_routes = routes['PATCH'].collect {|r| r.first.to_s}
+
+        {post: post_routes, get: get_routes, delete: delete_routes, options: options_routes, put: put_routes, patch: patch_routes}
+      end
+    end
+  end
+end
+
