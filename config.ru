310b484d1c4a71b12ac26eb477e29e3f15b74445
Felipe Rodrigues Michetti <felipe@codecode.com.br>
2018-02-24 16:48:58 -0300

    Refactoring

--- config.ru
+++ config.ru
@@ -9,8 +9,6 @@ require_relative 'api/app'
 
 ENV['TZ'] = 'America/Sao_Paulo'
 
-use Rack::Parser, content_types: {
-  'application/json' => proc { |body| ::MultiJson.decode body, :symbolize_keys => true }
-}
+use Rack::Parser, :content_types => {'application/json'  => Proc.new { |body| ::MultiJson.decode body, :symbolize_keys => true }}
 
 Rack::Handler.default.run(App, Port: 9494, Host: '0.0.0.0')
--- lib/utils/class_factory.rb
+++ lib/utils/class_factory.rb
@@ -21,7 +21,7 @@ module Utils
     SHOW_CONSTRAINTS = "SELECT table_name, constraint_type, constraint_name FROM information_schema.table_constraints where constraint_type IN ('PRIMARY KEY', 'FOREIGN KEY') AND table_name = '?' ORDER BY table_name, constraint_type;".freeze
 
     def class_from_string(str)
-      str.split('::').inject(Object) { |mod, class_name| mod.const_get(class_name) }
+      str.split('::').inject(Object) {|mod, class_name| mod.const_get(class_name)}
     end
 
     def get_table_constraints(connection, table_name)
@@ -29,16 +29,16 @@ module Utils
       ConnectionFactory.execute_query(connection, query.gsub('?', table_name), false)
     end
 
-    def create_classes(connection, module_constant)
+    def create_classes(connection, module_constant, name_convention = false)
       table_info = scan_classes(connection)
-      table_info.each { |t|
+      table_info.each {|t|
 
         table_name = t[:table_name]
         table_schema = t[:schema]
 
         table_constraints = get_table_constraints(connection, table_name)
-        t[:constraints] = table_constraints.map { |tc| {type: tc[:constraint_type], name: tc[:constraint_name]} }
-        primary_key_name = t[:constraints].detect { |tc| tc[:type].eql?('PRIMARY KEY') }
+        t[:constraints] = table_constraints.map {|tc| {type: tc[:constraint_type], name: tc[:constraint_name]}}
+        primary_key_name = t[:constraints].detect {|tc| tc[:type].eql?('PRIMARY KEY')}
 
         t[:columns_n_types] = get_table_columns(connection, table_name)
 
@@ -46,24 +46,26 @@ module Utils
         classes = []
 
         begin
-          Object.const_set(dynamic_name, Class.new(BaseModel) { |klass|
+          Object.const_set(dynamic_name, Class.new(BaseModel) {|klass|
             @db = connection
-            @require_valid_table = false
-            @primary_key = primary_key_name
-            @simple_table = table_name
-            @db_schema = table_schema
 
-            @table_name = table_name
-            @table_schema = table_schema
-            @fast_instance_delete_sql = ''
-            @fast_pk_lookup_sql = ''
+            unless name_convention
+              @require_valid_table = false
+              @primary_key = primary_key_name
+              @simple_table = table_name
+              @db_schema = table_schema
 
+              @table_name = table_name
+              @table_schema = table_schema
+              @fast_instance_delete_sql = ''
+              @fast_pk_lookup_sql = ''
 
-            set_dataset(connection[table_name.to_sym])
+              set_dataset(connection[table_name.to_sym])
+            end
 
             class << self
-              define_method('find_by_id') { |value| self[value] }
-              define_method('delete_by_id') { |value| self[value].destroy }
+              define_method('find_by_id') {|value| self[value]}
+              define_method('delete_by_id') {|value| self[value].destroy}
             end
 
             module_constant.const_set(dynamic_name, klass)
@@ -87,7 +89,7 @@ module Utils
     end
 
     def get_classes(module_instance)
-      module_instance.constants.select { |c| module_instance.const_get(c).is_a? Class }
+      module_instance.constants.select {|c| module_instance.const_get(c).is_a? Class}
     end
 
     def scan_classes(connection)
@@ -98,12 +100,11 @@ module Utils
 
     def scan_fields(connection, table_name)
       query = SHOW_COLUMNS.gsub('?', table_name)
-      ConnectionFactory.executar_query_sql(connection, query, false)
+      ConnectionFactory.execute_query(connection, query, false)
     end
 
-    private
     def snake_case_to_camel_case_name(string)
-      string = string.tr('_', ' ').split.map.with_index { |x, i| i.zero? ? x : x.capitalize }.join
+      string = string.tr('_', ' ').split.map.with_index {|x, i| i.zero? ? x : x.capitalize}.join
       string[0] = string[0].upcase
       string
     end
